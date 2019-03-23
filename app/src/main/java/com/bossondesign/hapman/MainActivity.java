94c64d67ed9c365bbf8cb6108884c4c33a6d229b
Sean Patterson <sean@bosson.design>
2019-03-22 20:17:52 -0400

    updated version number

--- app/src/main/java/com/bossondesign/hapman/MainActivity.java
+++ app/src/main/java/com/bossondesign/hapman/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         //test code for forcing links to open in app and not chrome - not working
         //MyWebClient webClient = new MyWebClient();
 
